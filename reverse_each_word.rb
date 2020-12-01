dd03c35407c3ef77722f8113c03478c80ec437f7
Louis Leffler <louisleffler@gmail.com>
2020-12-01 01:41:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -19,4 +19,12 @@ def reverse_each_word(string)
  
  
  
+ # reverse = ""
+ #ndex = 0 
+ #while index < string.length
+ #reverse = string[index] + reverse
+ #index += 1 
+ #end
+ #return reverse
+ #end 
  
