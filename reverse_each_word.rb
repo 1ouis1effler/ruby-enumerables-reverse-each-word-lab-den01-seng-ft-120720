4bf440257e678d052155bec07ac6aa2be194b70b
Louis Leffler <louisleffler@gmail.com>
2020-12-01 01:19:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
-def reverse_each_word
+def reverse_each_word(string)
   new_array = [ ]
   reverse_each_word.collect do |string|
     string = new_array
     new_array
   end 
+  
+  def test(sentence)
+  sentence.split.map {|word| word.reverse}.join(" ")
+end
