3acf319f9d86cc8fd8cb85a0be083b7aa06828db
Louis Leffler <louisleffler@gmail.com>
2020-12-01 01:15:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,6 @@
-def reverse_each_word("")
+def reverse_each_word("Hello there, and how are you?")
+  new_array = [ ]
+  reverse_each_word.collect do |string|
+    string = new_array
+    new_array
+  end 
