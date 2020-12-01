dbde18a41523c8a41aeafdb00f7c21244e96b4c5
Louis Leffler <louisleffler@gmail.com>
2020-12-01 02:02:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,4 @@
 def reverse_each_word(string)
- new_string = [ ]
- string.collect
- 
- 
- 
- 
- 
- 
- 
- #Write a method called reverse_each_word that takes in a string argument of a sentence and returns that same sentence with each word #reversed in place.
-#First solve it using .each Then utilize the same method using .collect to see the difference.
- 
- 
- 
  reverse = ""
  index = 0 
  while index < string.length
@@ -21,4 +7,12 @@ def reverse_each_word(string)
  end
  return reverse
  end 
+ 
+ 
+ 
+ 
+ 
+ #Write a method called reverse_each_word that takes in a string argument of a sentence and returns that same sentence with each word #reversed in place.
+#First solve it using .each Then utilize the same method using .collect to see the difference.
+ 
  
