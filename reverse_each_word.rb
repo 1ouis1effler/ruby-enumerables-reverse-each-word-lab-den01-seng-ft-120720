d1eb9643672f06eb6797e5725a079dc4ea337e11
Louis Leffler <louisleffler@gmail.com>
2020-12-01 02:02:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,12 +13,12 @@ def reverse_each_word(string)
  
  
  
- # reverse = ""
- #ndex = 0 
- #while index < string.length
- #reverse = string[index] + reverse
- #index += 1 
- #end
- #return reverse
- #end 
+ reverse = ""
+ index = 0 
+ while index < string.length
+ reverse = string[index] + reverse
+ index += 1 
+ end
+ return reverse
+ end 
  
